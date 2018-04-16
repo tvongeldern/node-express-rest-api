8dbce4786a0a89e7b4df5fedec27591d0088f9c0
Tom von Geldern <tvg@bookcameo.com>
2018-04-16 18:00:39 -0500

    Implementing Prettier.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // EXPRESS SETUP
 import express from 'express';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
 const app = express();
 app.use(bodyParser.json());
 
@@ -11,31 +11,32 @@ import * as endpointsModule from 'endpoints';
 // DECLARING CONSTANTS
 import packageJson from './package';
 const PORT = 3000;
-const apiVersion = packageJson.version.split('.').slice(0, -1).join('.');
+const apiVersion = packageJson.version
+	.split('.')
+	.slice(0, -1)
+	.join('.');
 const endpointGroups = Object.keys(endpointsModule);
 
 // MOUNTING ENDPOINTS
 console.log('\nMOUNTING ENDPOINTS...\n');
 
 endpointGroups.forEach((endpointGroupName) => {
-    console.log(endpointGroupName);
-    const endpoints = endpointsModule[endpointGroupName];
-    const endpointNames = Object.keys(endpoints);
-    endpointNames.forEach((endpointName) => {
-        console.log(endpointName);
-        const name = `${endpointGroupName}/${endpointName}`;
-        const endpointModule = endpoints[endpointName];
-        const endpoint = new Endpoint({...endpointModule, name });
-        app[endpoint.method](`/${apiVersion}/${name}${endpoint.reference ? '/:reference' : ''}`, endpoint.handler);
-    });
+	const endpoints = endpointsModule[endpointGroupName];
+	const endpointNames = Object.keys(endpoints);
+	endpointNames.forEach((endpointName) => {
+		const name = `${endpointGroupName}/${endpointName}`;
+		const endpointModule = endpoints[endpointName];
+		const endpoint = new Endpoint({ ...endpointModule, name });
+		app[endpoint.method](`/${apiVersion}/${name}${endpoint.reference ? '/:reference' : ''}`, endpoint.handler);
+	});
 });
 
 console.log('\nFinished mounting endpoints!\n');
 
 // BOOTING API
 app.listen(PORT, (err) => {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(`\nAPI version ${apiVersion} started on port ${PORT}\n`);
+	if (err) {
+		return console.log(err);
+	}
+	console.log(`\nAPI version ${apiVersion} started on port ${PORT}\n`);
 });
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,25 +1,22 @@
 export default {
-    entry: `${__dirname}/index.js`,
-    output: {
-        path: `${__dirname}/dist`,
-        filename: 'server.js',
-    },
-    mode: 'development', // To preserve stack traces for error messages
-    target: 'node',
-    resolve: {
-        modules: [
-            'lib',
-            'node_modules',
-        ],
-        extensions: [ '.json', '.js' ],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader",
-                exclude: /(node_modules)/,
-            },
-        ],
-    },
+	entry: `${__dirname}/index.js`,
+	output: {
+		path: `${__dirname}/dist`,
+		filename: 'server.js',
+	},
+	mode: 'development', // To preserve stack traces for error messages
+	target: 'node',
+	resolve: {
+		modules: ['lib', 'node_modules'],
+		extensions: ['.json', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /(node_modules)/,
+			},
+		],
+	},
 };
